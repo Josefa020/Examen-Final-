14d5cef71f0d23bd25b4f0f0644e99a361d07e8d
Josefa Pérez <jprep@uc.cl>
2025-11-28 14:59:59 -0300

    Add files via upload

---
+++ proyectos.js
@@ -0,0 +1,69 @@
+
+const datosProyectos = [
+  {
+    "id": "2025",
+    "titulo": "Noctu",
+    "subtitulo": "Industrial",
+    "enlace": "proyecto-1.html",
+    "imagen_url": "assets/proyecto1.jpg"
+  },
+  {
+    "id": "2025",
+    "titulo": "Alicate Universal",
+    "subtitulo": "Ergonomía",
+    "enlace": "proyecto-2.html",
+    "imagen_url": "assets/proyecto2.jpg"
+  },
+  {
+    "id": "2025",
+    "titulo": "Co-florecer",
+    "subtitulo": "Estratégico",
+    "enlace": "proyecto-3.html",
+    "imagen_url": "assets/proyecto3.jpg"
+  },
+  {
+    "id": "2025",
+    "titulo": "Chile en 5 pilares",
+    "subtitulo": "Estratégico",
+    "enlace": "proyecto-4.html",
+    "imagen_url": "assets/proyecto4.jpg"
+  },
+  {
+    "id": "2024",
+    "titulo": "Akutun",
+    "subtitulo": "Souvenir",
+    "enlace": "proyecto-5.html",
+    "imagen_url": "assets/proyecto5.jpg"
+  },
+  {
+    "id": "2025",
+    "titulo": "El cuerpo paciente",
+    "subtitulo": "Crítico",
+    "enlace": "proyecto-6.html",
+    "imagen_url": "assets/proyecto6.jpg"
+  }
+];
+
+document.addEventListener('DOMContentLoaded', () => {
+    
+    const contenedor = document.getElementById('proyectos-contenedor');
+    
+    // Aquí usamos la variable local, ya no necesitamos fetch().
+    datosProyectos.forEach(proyecto => {
+        
+        const proyectoLink = document.createElement('a');
+        proyectoLink.href = proyecto.enlace;
+        proyectoLink.className = 'proyecto-item';
+        proyectoLink.style.backgroundImage = `url('${proyecto.imagen_url}')`;
+        
+        proyectoLink.innerHTML = `
+            <div class="overlay">
+                <h3>${proyecto.id.toUpperCase()}: ${proyecto.titulo}</h3>
+                <p class="etiqueta">${proyecto.subtitulo}</p>
+            </div>
+        `;
+        
+        contenedor.appendChild(proyectoLink);
+    });
+
+});
